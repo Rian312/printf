151151d19ee06ee1e8769009078415b6d29c9b00
Moniqchege <quichege33@gmail.com>
2023-10-17 09:38:21 -0700

    C-Printf

--- handle_print.c
+++ handle_print.c
@@ -13,7 +13,8 @@
  * Return: 1 0r 2;
  */
 
-int handle_print(const char *fmt, int *ind, va_list, char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[], 
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -21,9 +22,9 @@ int handle_print(const char *fmt, int *ind, va_list, char buffer[], int flags, i
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13striing}, {'\0' NULL}
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types.fmt != '\0'; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
@@ -31,11 +32,15 @@ int handle_print(const char *fmt, int *ind, va_list, char buffer[], int flags, i
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, " ", 1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
