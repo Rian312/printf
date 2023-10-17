3f374d6608ca03e92897e246cd044c858156e549
Moniqchege <quichege33@gmail.com>
2023-10-17 11:07:43 -0700

    C-Printf

--- handle_print.c
+++ handle_print.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * handle_print - prints an argument based on its type
- * @fmt: formatted string in which to print the arguments
- * @list: list of arguments to be printed.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments
+ * @list: List of arguments to be printed
  * @ind: ind.
- * @buffer: buffer array to handle print.
- * @flags: calculates active flags.
- * @width: get width
- * @precision: precision specification
- * @size: size specifier
- * Return: 1 0r 2;
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: Get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ *
+ * Return: 1 or 2
  */
-
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], 
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
@@ -42,6 +42,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			if (fmt[*ind] == ' ')
 				--(*ind);
+
 			return (1);
 		}
 		unknow_len += write(1, &fmt[*ind], 1);
